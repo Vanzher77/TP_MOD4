da5f8a470635ec171c27420148bff90bcef2202d
Vanzher77 <121426519+Vanzher77@users.noreply.github.com>
2023-03-13 00:27:07 +0700

    menambahkan implementasi dengan state-based

--- TP_MOD4/TP_MOD4/Program.cs
+++ TP_MOD4/TP_MOD4/Program.cs
@@ -12,4 +12,68 @@ public class KodePos
     }
 }
 
+public class DoorMachine
+{
+    //Membuat State-based construction
+    enum state { terkunci, terbuka};
+    
+    public static void Main()
+    {
+        //Menampilkan nama kelurahan dan kode pos kelurahan tersebut
+        Console.WriteLine("Kelurahan: " + KodePos.kelurahan.Batununggal);
+        Console.WriteLine("Kodepos: " + KodePos.getKodePos(KodePos.kelurahan.Batununggal));
+        Console.WriteLine();
 
+        state state1 = state.terkunci;
+        //membuat perintah menggunakan string
+        string[] kondisi = { "Pintu terkunci", "Pintu Terbuka" };
+        while(state1 != null)
+        {
+            Console.WriteLine(kondisi[(int)state1]);
+            Console.WriteLine();
+            Console.WriteLine("Masukan Perintah: ");
+
+            string perintah = Console.ReadLine();
+            //membuat perintah dengan menggunakan if else
+            switch (state1)
+            {
+                //membuat case terkunci
+                case state.terkunci:
+                    if(perintah == "Kunci Pintu")
+                    {
+                        state1 = state.terkunci;
+                    }
+                    else if(perintah == "Buka pintu")
+                    {
+                        state1 = state.terbuka;
+                    }
+                    else
+                    {
+                        state1 = state.terkunci;
+                        Console.WriteLine("Perintah Salah");
+                    }
+                    Console.WriteLine();
+                    //break agar perulangan berhenti saat menjalankan perintah
+                    break;
+                //membuat case terbuka
+                case state.terbuka:
+                    if(perintah == "Kunci Pintu")
+                    {
+                        state1 = state.terkunci;
+                    }
+                    else if(perintah == "Buka Pintu")
+                    {
+                        state1 = state.terbuka;
+                    }
+                    else
+                    {
+                        state1 = state.terbuka;
+                        Console.WriteLine("Perintah Salah");
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
+                    break;
+            }
+        }
+    }
+}
