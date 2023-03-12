6f6bb3cd285f82a6e38ee03deb2f16877f9d7f32
Vanzher77 <121426519+Vanzher77@users.noreply.github.com>
2023-03-13 00:25:38 +0700

    menambahkan implementasi dengan table-driven

--- TP_MOD4/TP_MOD4/Program.cs
+++ TP_MOD4/TP_MOD4/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+public class KodePos
+{   
+    public enum kelurahan { Batununggal, Kujangsari, Mengger, Weters, Cijaura, Jatisari, Margasari, Sekejati, Kebonwaru, Maleer, Samoja}
+    //membuat table driven 
+    public static int getKodePos(kelurahan kel)
+    {
+        //membuat array dengan mengisi array dengan kodepos
+        int[] kodepos = { 40266, 40287, 40267, 40256, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+        return kodepos[(int) kel];
+    }
+}
+
+
